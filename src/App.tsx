3f9f31425d23b94df3d3cc87eca990b56925c1bf
Mads E Kirchhoff <mads.kirchhoff@kauza.dk>
2024-12-13 16:17:44 +0100

    solid

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import './App.css'
-import { createSignal } from 'solid-js'
+import "./App.css";
+import { createSignal, Show, Suspense } from "solid-js";
 // @ts-expect-error Unable to infer type at the moment
-import solidLogo from './assets/solid.svg'
-import Day12 from "./Day12.tsx";
+import solidLogo from "./assets/solid.svg";
+import { Day12 } from "./Day12.tsx";
+import { fileToMatrix } from "./helpers.ts";
+import { createResource } from "solid-js";
+
+const fetchMatrix = async (filename: number) => await fileToMatrix(filename);
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [day, dayset] = createSignal(12);
+  const [matrix] = createResource(day, fetchMatrix);
 
   return (
-    <div class="App">App
-<Day12 />
+    <div class="App">
+      <Show when={matrix()}>{value => <Day12 data={value()} />}</Show>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Day12.tsx
+++ src/Day12.tsx
@@ -1,10 +1,85 @@
+import { createResource, lazy, Suspense } from "solid-js";
+import { sum } from "https://deno.land/x/sum/mod.ts";
+import {
+  addCoordinates,
+  type Coordinate,
+  directionsVector,
+  fileToMatrix,
+  forEachCoordinate,
+  getMatrixValue,
+  getMatrixValueXY,
+  mapMatrix,
+} from "./helpers.ts";
 
-function Day12() {
+
+export const Day12 = (props: {data: string[][]}) => {
+
 
   return (
-    <div>day12
-    </div>
-  )
-}
+<div>fje</div>
+  );
+};
 
-export default Day12
+// const Matrix = asu() => {
+
+//   const { matrixXY } = await fileToMatrix("data12");
+//   return <div>{matrixXY}</div>;})
+  // const explored: (boolean | string)[][] = mapMatrix(matrixXY, (_) => "");
+
+  // const recursiveExploreRegion = (
+  //   currentCoordinate: Coordinate,
+  //   letter: string,
+  //   area: number,
+  //   perimeter: number,
+  // ): { area: number; perimeter: number } => {
+  //   const [x, y] = currentCoordinate;
+  //   // console.log(x, y);
+  //   const cell = getMatrixValue(currentCoordinate);
+  //   if (!cell) {
+  //     return { area, perimeter: perimeter + 1 };
+  //   }
+  //   if (cell !== letter) {
+  //     return { area, perimeter: perimeter + 1 };
+  //   }
+  //   if (explored[x]?.[y]) {
+  //     return { area, perimeter: perimeter };
+  //   }
+  //   explored[x][y] = letter;
+  //   let newArea = area + 1;
+  //   let newPerimeter = perimeter;
+  //   for (const direction of directionsVector) {
+  //     const newCoord = addCoordinates(direction, currentCoordinate);
+  //     const { area: cArea, perimeter: cPerimeters } = recursiveExploreRegion(
+  //       newCoord,
+  //       letter,
+  //       0,
+  //       0,
+  //     );
+  //     newArea += cArea;
+  //     newPerimeter += cPerimeters;
+  //   }
+  //   return { area: newArea, perimeter: newPerimeter /* TODO: double check */ };
+  // };
+
+  // let result = 0;
+  // forEachCoordinate(matrixXY, ([x, y]) => {
+  //   if (explored[x][y]) {
+  //     return;
+  //   }
+  //   const currentLetter = getMatrixValue([x, y]);
+  //   const { area, perimeter } = recursiveExploreRegion(
+  //     [x, y],
+  //     currentLetter,
+  //     0,
+  //     0,
+  //   );
+  //   result += area * perimeter;
+  //   console.log(currentLetter, [x, y], area, perimeter, area * perimeter);
+  // });
+  // console.log(result);
+
+  // return (
+  //   <div>
+  //     <h1>Matrix</h1>
+  //   </div>
+  // );
